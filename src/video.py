d707fb246eaf173be2a87764c8dad16d8357dcb7
Arlen Asanov <a.asanov2610@gmail.com>
2023-09-06 15:22:51 +0300

    add homework_6 solution

--- src/video.py
+++ src/video.py
@@ -8,12 +8,25 @@ class Video:
     youtube = build("youtube", "v3", developerKey=api_key)
 
     def __init__(self, id_video: str):
-        self.id_video = id_video
-        self.channel = self.youtube.videos().list(id=self.id_video, part='snippet,statistics').execute()
-        self.url = f"https://www.youtube.com/channel/{self.id_video}"
-        self.title = self.channel["items"][0]["snippet"]["title"]
-        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
-        self.like = self.channel["items"][0]["statistics"]["likeCount"]
+        try:
+            if any(map(str.isdigit, id_video)):
+                self.channel_id = id_video
+            else:
+                self.channel_info = None
+                self.title = None
+                self.url = None
+                self.view_count = None
+                self.like_count = None
+                raise ValueError
+        except ValueError:
+            print("Неверный ID")
+        else:
+            self.id_video = id_video
+            self.channel = self.youtube.videos().list(id=self.id_video, part='snippet,statistics').execute()
+            self.url = f"https://www.youtube.com/channel/{self.id_video}"
+            self.title = self.channel["items"][0]["snippet"]["title"]
+            self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+            self.like = self.channel["items"][0]["statistics"]["likeCount"]
 
     def __str__(self):
         return self.title
